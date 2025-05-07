b062f61e2e6f25e23477d64aa13bed2a401e6530
AbdoEssam2024 <eabdo2051@gmail.com>
2025-05-07 07:30:37 +0300

    fix db_config

--- database_config.py
+++ database_config.py
@@ -9,7 +9,7 @@ host = "fdb1030.awardspace.net"
 database = "4586599_resturrant"
 port = "3306"
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://4586599_resturrant:Jory@100124@fdb1030.awardspace.net:3306/4586599_resturrant"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
