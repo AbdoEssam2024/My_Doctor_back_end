45b3b7279142476fdc453bd6623361de3016cb68
AbdoEssam2024 <eabdo2051@gmail.com>
2025-05-07 07:15:49 +0300

    fix requirements

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from controller.user_controller import login
 app = FastAPI()
 
 
+@app.get("/")
+def home():
+    return {"msg": "Hello from Railway!"}
+
+
 @app.post("/login")
 async def userLogin(username: str, password: str):
     return await login(username, password)
